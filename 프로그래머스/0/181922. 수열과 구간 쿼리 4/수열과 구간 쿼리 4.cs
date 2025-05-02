71c0681d5f7b04c925b904b9baca4851d4375615
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-05-02 22:08:17 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.85 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        int[] answer = arr;
+        
+        for (int j = 0; j < queries.GetLength(0); j++)
+        {
+            int s = queries[j, 0];
+            int e = queries[j, 1];
+            int k = queries[j, 2];
+            
+            for (int i = s; i <= e; i++)
+            {
+                if (i % k == 0)
+                {
+                    answer[i] += 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
